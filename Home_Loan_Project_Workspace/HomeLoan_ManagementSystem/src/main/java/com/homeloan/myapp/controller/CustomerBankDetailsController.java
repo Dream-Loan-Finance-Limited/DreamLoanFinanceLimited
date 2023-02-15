c0bd8145808f943c1f46b327fe7d2626278c328a
rathodrituja9595 <rathodrutuja2020@gmail.com>
2023-02-15 16:48:58 +0530

    abc

--- Home_Loan_Project_Workspace/HomeLoan_ManagementSystem/src/main/java/com/homeloan/myapp/controller/CustomerBankDetailsController.java
+++ Home_Loan_Project_Workspace/HomeLoan_ManagementSystem/src/main/java/com/homeloan/myapp/controller/CustomerBankDetailsController.java
@@ -3,6 +3,7 @@ package com.homeloan.myapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class CustomerBankDetailsController {
 	@Autowired
 	private CustomerBankDetailsServiceI customerBankDetailsI;
 
+	@PostMapping("/savecustomerBankDetails")
 	public ResponseEntity<CustomerBankDetails> saveCustomerBankDetails(
 			@RequestBody CustomerBankDetails customerBankDetails) {
 		CustomerBankDetails save = customerBankDetailsI.saveCustomerBankDetails(customerBankDetails);
--- Home_Loan_Project_Workspace/HomeLoan_ManagementSystem/src/main/java/com/homeloan/myapp/serviceImpl/CustomerBankDetailsServiceImpl.java
+++ Home_Loan_Project_Workspace/HomeLoan_ManagementSystem/src/main/java/com/homeloan/myapp/serviceImpl/CustomerBankDetailsServiceImpl.java
@@ -1,5 +1,22 @@
 package com.homeloan.myapp.serviceImpl;
 
-public class CustomerBankDetailsServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.homeloan.myapp.entities.CustomerBankDetails;
+import com.homeloan.myapp.repository.CustomerBankDetailsRepository;
+import com.homeloan.myapp.serviceI.CustomerBankDetailsServiceI;
+
+@Service
+public class CustomerBankDetailsServiceImpl implements CustomerBankDetailsServiceI{
+
+	@Autowired
+	private CustomerBankDetailsRepository customerBankDetailsRepository;
+	
+	
+	@Override
+	public CustomerBankDetails saveCustomerBankDetails(CustomerBankDetails customerBankDetails) {
+		return customerBankDetailsRepository.save(customerBankDetails);
+	}
 
 }
